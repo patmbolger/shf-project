b18e51a32f166bee4be1792016ae72f220f49c52
Patrick Bolger <patmbolger@gmail.com>
2017-05-08 15:21:39 -0400

    fixed translations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418213009) do
+ActiveRecord::Schema.define(version: 20170507103334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,13 @@ ActiveRecord::Schema.define(version: 20170418213009) do
 
   create_table "kommuns", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "member_pages", force: :cascade do |t|
+    t.string   "filename",   null: false
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
