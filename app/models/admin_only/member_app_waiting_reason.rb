1d3a786256b7aef24df717f867985d2d0af467b0
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-06-21 01:28:45 -0700

    pass locale to other_reason_name/_desc to make seeding easier

--- app/models/admin_only/member_app_waiting_reason.rb
+++ app/models/admin_only/member_app_waiting_reason.rb
@@ -12,13 +12,23 @@ module AdminOnly
 
 
     # This is effectively a CONSTANT that is used in the UI
-    def self.other_reason_name
-      I18n.t('admin_only.member_app_waiting_reasons.other_custom_reason')
+    def self.other_reason_name(locale = I18n.locale)
+      I18n.t('admin_only.member_app_waiting_reasons.other_custom_reason', locale)
     end
+
+
+    def self.other_reason_desc(locale = I18n.locale)
+      I18n.t('admin_only.member_app_waiting_reasons.other_custom_reason_desc', locale)
+    end
+
 
 
     def self.default_name_method
       :name_sv
+    end
+
+    def self.default_description_method
+      :description_sv
     end
 
   end
