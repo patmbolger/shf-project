4dd87d23fb541add874e0be309fa2fb94d8696c4
Patrick Bolger <patmbolger@gmail.com>
2017-06-27 05:51:20 -0400

    removed selenium

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,12 +2,7 @@ Before('@javascript, @poltergeist') do
   Capybara.current_driver = :poltergeist
 end
 
-Before('@selenium') do
-  # For JS-driven tests that Poltergeist does not handle cleanly.
-  Capybara.javascript_driver = :selenium
-end
-
-After('@javascript, @poltergeist, @selenium') do
+After('@javascript, @poltergeist') do
   Capybara.reset_sessions!
   Capybara.current_driver = :rack_test
 end
