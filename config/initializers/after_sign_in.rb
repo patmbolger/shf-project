39bf3d886c66479eb69545ec68ca76fe41110679
Patrick Bolger <patmbolger@gmail.com>
2017-12-12 13:55:28 -0500

    hound suggestions

--- config/initializers/after_sign_in.rb
+++ config/initializers/after_sign_in.rb
@@ -1,4 +1,4 @@
-Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
+Warden::Manager.after_set_user except: :fetch do |user, _auth, _opts|
   if user&.member? && ! user.membership_current?
     user.update(member: false)
   end
