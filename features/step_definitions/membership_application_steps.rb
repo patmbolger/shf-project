2289e23a835a91a3b6a4361c51795fbdb03d740b
Patrick Bolger <patmbolger@gmail.com>
2019-03-26 09:06:28 -0400

    add files count to apps index view

--- features/step_definitions/membership_application_steps.rb
+++ features/step_definitions/membership_application_steps.rb
@@ -101,3 +101,17 @@ When "I select files delivery radio button {capture_string}" do |option|
   page.evaluate_script("$('.app-submit').prop('disabled', false)")
 
 end
+
+And "I should see {capture_string} files for the {capture_string} listed application" do |count, ordinal|
+  # Use to confirm uploaded files count in ShfApplication index view
+  # If more than one app then make sure the sort order supports the test step.
+  # Examples:
+  #  "I should see 0 files for the first listed application"
+  #  "I should see 3 files for the second listed application"
+
+  index = ordinal ? [0, 1, 2, 3, 4].send(ordinal.lstrip) : 0
+
+  ele = all('#shf_applications_list table tr > td.number_of_files')[0]
+
+  expect(ele.text).to eq count
+end
