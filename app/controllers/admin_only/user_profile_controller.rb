fef3a3009b225d2a94e080e700f57956dcdbed40
Patrick Bolger <patmbolger@gmail.com>
2019-04-03 15:29:07 -0400

    handle setting membership_number to empty string

--- app/controllers/admin_only/user_profile_controller.rb
+++ app/controllers/admin_only/user_profile_controller.rb
@@ -8,7 +8,11 @@ module AdminOnly
     def become
       return unless current_user.admin?
 
-      bypass_sign_in(User.find(params[:id]))
+      new_user = User.find(params[:id])
+
+      bypass_sign_in(new_user)
+
+      current_user = new_user # neccessary so feature test passes
 
       helpers.flash_message(:warn, t('.have_become', user_id: @user.id))
 
@@ -43,10 +47,22 @@ module AdminOnly
         params[:user].delete(:password_confirmation)
       end
 
+      # if membership_number is blank then set to nil.
+      # The User model allows for blank membership_number.
+      # However, the DB (PG) has a unique constraint on this attribute and
+      # hence will raise an exception if the attribute is an empty string
+      # (one of which will already exist in the DB).
+      # Since that exception occurs after model validation, the "update" method
+      # cannot rescue this exception. (the DB considers "NULL" values unequal,
+      # so "nil" will not trigger the exception on save.)
+
+      if params[:user][:membership_number].blank?
+        params[:user][:membership_number] = nil
+      end
+
       params.require(:user).permit(:first_name, :last_name, :email, :password,
                                    :password_confirmation, :membership_number,
-                                   shf_application_attributes: [ :contact_email ])
+                                   shf_application_attributes: [ :contact_email, :id ])
     end
   end
-
 end
