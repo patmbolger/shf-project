d8911e9910ef24acbdffeae2252de5076dc2c1f6
Patrick Bolger <patmbolger@gmail.com>
2019-05-14 07:25:23 -0400

    switch to webdrivers gem

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,7 @@ require 'cucumber/timecop'
 require 'cucumber/rspec/doubles'
 require 'capybara/poltergeist'
 require 'email_spec/cucumber'
+require 'webdrivers/chromedriver'
 
 # Put the Geocoder into test mode so no actual API calls are made and stub with fake data
 require_relative '../../spec/support/geocoder'
