18103fb8147ce25de7a596bb6c74627f9cd9e119
Patrick Bolger <patmbolger@gmail.com>
2019-06-02 17:57:58 -0400

    cleaned up

--- app/controllers/shf_applications_controller.rb
+++ app/controllers/shf_applications_controller.rb
@@ -113,22 +113,10 @@ class ShfApplicationsController < ApplicationController
         helpers.flash_message(:notice, t('.success'))
       end
 
-      respond_to do |format|
-        format.html { redirect_to define_path(evaluate_update(params)) }
-        format.js do
-          uploaded_html = render_to_string(partial: 'uploaded_files_list',
-                                           locals: { shf_application: @shf_application })
-          render json: { uploaded_html: uploaded_html, status: :ok }
-        end
-      end
-      # redirect_to define_path(evaluate_update(params))
+      redirect_to define_path(evaluate_update(params))
     else
-      respond_to do |format|
-        format.html { create_or_update_error(t('.error'), companies_and_numbers, numbers_str, :edit) }
-        format.js { render json: { status: :unprocessable_entity } }
-      end
 
-      # create_or_update_error(t('.error'), companies_and_numbers, numbers_str, :edit)
+      create_or_update_error(t('.error'), companies_and_numbers, numbers_str, :edit)
     end
   end
 
@@ -142,7 +130,6 @@ class ShfApplicationsController < ApplicationController
           render json: { uploaded_html: uploaded_html, status: :ok }
         end
       end
-      # redirect_to define_path(evaluate_update(params))
     else
       respond_to do |format|
         format.js { render json: { status: :unprocessable_entity } }
