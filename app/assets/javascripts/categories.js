c51eb2def16e035dda3f93095cb00da435bdcde1
Patrick Bolger <patmbolger@gmail.com>
2019-06-20 07:27:40 -0400

    cleaned up JS categories namespace

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,91 +1,92 @@
-$(function() {
-  'use strict';
+var categories = {
 
-  $('body').on('ajax:complete', '.delete-category', removeCategory);
+  init: function() {
+    'use strict';
 
-  $('body').on('ajax:complete', '.edit-category-button', showEditRow);
+    $('body').on('ajax:complete', '.delete-category', categories.removeCategory);
 
-  $('body').on('ajax:complete', '.edit-category-cancel-button', showDisplayRow);
+    $('body').on('ajax:complete', '.edit-category-button', categories.showEditRow);
 
-  $('body').on('ajax:complete', '.edit-category-form', handleFormResults);
+    $('body').on('ajax:complete', '.edit-category-cancel-button', categories.showDisplayRow);
 
-  $('#new-category-button').on('ajax:complete', showNewCategoryRow);
+    $('body').on('ajax:complete', '.edit-category-form', categories.handleFormResults);
 
-  $('body').on('click', '.new-category-cancel-button', removeNewCategoryRow);
+    $('#new-category-button').on('ajax:complete', categories.showNewCategoryRow);
 
-});
+    $('body').on('click', '.new-category-cancel-button', categories.removeNewCategoryRow);
+  },
 
-function showEditRow(e, response) {
-  if (Utility.checkHttpError(response) === false) {
-    var data = JSON.parse(response.responseText);
+  showEditRow: function(e, response) {
+    if (Utility.checkHttpError(response) === false) {
+      var data = JSON.parse(response.responseText);
 
-    // Receiving an edit row from server - replace display row with that
-    var $displayRow = $('#category-display-row-' + data.business_category_id);
+      // Receiving an edit row from server - replace display row with that
+      var $displayRow = $('#category-display-row-' + data.business_category_id);
 
-    $displayRow.replaceWith(data.edit_row);
-  }
-}
-
-function showDisplayRow(e, response) {
-  if (Utility.checkHttpError(response) === false) {
-    var data = JSON.parse(response.responseText);
-
-    // Receiving display row from server - replace display row with that
-    var $editRow = $('#category-edit-row-' + data.business_category_id);
-
-    $editRow.replaceWith(data.display_row);
-  }
-}
-
-function showNewCategoryRow(e, response) {
-  if (Utility.checkHttpError(response) === false) {
-    var data = JSON.parse(response.responseText);
-
-    // Receiving new-cat row from server - add to top of categories table
-    $('#business_categories').prepend(data.new_row);
-  }
-}
-
-function removeNewCategoryRow(e, response) {
-  removeThisRow(this);
-}
-
-function removeThisRow(this_one) {
-  // Remove the parent table row within which this row exists
-  $(this_one).closest('tr.category-edit-row').remove();
-}
-
-function handleFormResults(e, response) {
-  if (Utility.checkHttpError(response) === false) {
-    var data = JSON.parse(response.responseText);
-
-    if (Utility.checkActionError(response, data) === false) {
-      // Receiving a display row from server
-      // If we find a matching edit row, replace that with the display row
-      // If not, the display row is for a *new* category - prepend to table.
-
-      var $editRow = $('#category-edit-row-' + data.business_category_id);
-      if ($editRow.length == 1) {
-        $editRow.replaceWith(data.display_row);
-      } else {
-        $('#business_categories').prepend(data.display_row);
-
-        // Remove the edit row where the category was created
-        removeThisRow(this);
-      }
-
-    } else {
-      $('#category-edit-errors').html(data.errors);
+      $displayRow.replaceWith(data.edit_row);
     }
-  }
-}
+  },
 
-function removeCategory(e, response) {
+  showDisplayRow: function(e, response) {
+    if (Utility.checkHttpError(response) === false) {
+      var data = JSON.parse(response.responseText);
 
-  if (Utility.checkHttpError(response) === false) {
-    var data = JSON.parse(response.responseText);
+      // Receiving display row from server - replace display row with that
+      var $editRow = $('#category-edit-row-' + data.business_category_id);
 
-    // remove row containing the 'delete-category' link
-    $(e.target.closest('tr')).fadeOut(800);
+      $editRow.replaceWith(data.display_row);
+    }
+  },
+
+  showNewCategoryRow: function(e, response) {
+    if (Utility.checkHttpError(response) === false) {
+      var data = JSON.parse(response.responseText);
+
+      // Receiving new-cat row from server - add to top of categories table
+      $('#business_categories').prepend(data.new_row);
+    }
+  },
+
+  removeNewCategoryRow: function(e, response) {
+    categories.removeThisRow(this);
+  },
+
+  removeThisRow: function(this_one) {
+    // Remove the parent table row within which this row exists
+    $(this_one).closest('tr.category-edit-row').remove();
+  },
+
+  handleFormResults: function(e, response) {
+    if (Utility.checkHttpError(response) === false) {
+      var data = JSON.parse(response.responseText);
+
+      if (Utility.checkActionError(response, data) === false) {
+        // Receiving a display row from server
+        // If we find a matching edit row, replace that with the display row
+        // If not, the display row is for a *new* category - prepend to table.
+
+        var $editRow = $('#category-edit-row-' + data.business_category_id);
+        if ($editRow.length == 1) {
+          $editRow.replaceWith(data.display_row);
+        } else {
+          $('#business_categories').prepend(data.display_row);
+
+          // Remove the edit row where the category was created
+          categories.removeThisRow(this);
+        }
+
+      } else {
+        $('#category-edit-errors').html(data.errors);
+      }
+    }
+  },
+
+  removeCategory: function(e, response) {
+    if (Utility.checkHttpError(response) === false) {
+      var data = JSON.parse(response.responseText);
+
+      // remove row containing the 'delete-category' link
+      $(e.target.closest('tr')).fadeOut(800);
+    }
   }
 }
