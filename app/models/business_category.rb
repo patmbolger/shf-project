1852c28789fad979c6c7227e04698ef1c9efe33d
Patrick Bolger <patmbolger@gmail.com>
2020-05-29 08:39:38 -0400

    scope search_for

--- app/models/business_category.rb
+++ app/models/business_category.rb
@@ -5,4 +5,18 @@ class BusinessCategory < ApplicationRecord
 
   has_and_belongs_to_many :shf_applications
   has_many :companies, through: :shf_applications
+
+  def self.for_search
+    categories = roots.order(:name).to_a
+    categories.dup.each_with_index do |category, idx|
+      categories.insert(idx+1, category.children)
+    end
+    categories.flatten
+  end
+
+  def search_name
+    return name if is_root?
+
+    parent.name + ' >> ' + name
+  end
 end
