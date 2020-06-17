dfc9e9f123f0def2d953e405cb4221d63645d66f
Patrick Bolger <patmbolger@gmail.com>
2020-06-17 09:05:51 -0400

    remove uniqueness test, BusinessCategory

--- spec/models/business_category_spec.rb
+++ spec/models/business_category_spec.rb
@@ -37,7 +37,6 @@ RSpec.describe BusinessCategory, type: :model do
 
   describe 'Validations' do
     it { is_expected.to validate_presence_of :name }
-    it { is_expected.to validate_uniqueness_of :name }
   end
 
   describe '.for_search' do
